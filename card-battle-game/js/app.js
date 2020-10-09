3f8a86f4f1158938f75ccada9bccb5e78885f1a8
Jeremy <jeremy.spewock@gmail.com>
2020-10-09 16:32:08 -0400

    set up API to retrieve cards

--- card-battle-game/js/app.js
+++ card-battle-game/js/app.js
@@ -1,4 +1,13 @@
 console.log('connected')
+let dexNum = Math.ceil(Math.random()*890)
 $(() => {
     console.log('j query connected')
+    $.ajax({
+        url: `https://api.pokemontcg.io/v1/cards?nationalPokedexNumber=${dexNum}`
+    }).then((data) => {
+        console.log(data)
+        $('body').html(
+            `<img src="${data.cards[0].imageUrl}">`
+        )
+    })
 })
