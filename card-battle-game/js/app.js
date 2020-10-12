c6eb4e15a0c5d126057eed68a065563a17f158e1
Jeremy <jeremy.spewock@gmail.com>
2020-10-12 12:48:11 -0400

    added row divs for later styling

--- card-battle-game/js/app.js
+++ card-battle-game/js/app.js
@@ -1,7 +1,10 @@
-console.log('connected')
+// console.log('connected')
 $(() => {
     const cards = []
-    console.log('j query connected')
+    let pChoice = ''
+    // console.log('j query connected')
+    const startGame = () => {
+
     for (let i = 0; i < 10; i++) {
     let dexNum = Math.ceil(Math.random()*890) //Random pokedex number to generate pokemon
     $.ajax({
@@ -17,14 +20,30 @@ $(() => {
             newCard.addClass('player-card')
         }
         $('body').append(newCard)
-        console.log(cards)
-        //// Testing variable idea
-        let pChoice = ''
-        $('img').on('click', () => {
-            console.log($(event.target).attr('src'))
-            pChoice = cards.find(({imageUrl}) => imageUrl === $(event.target).attr('src'))
-            console.log(pChoice)
-        })
+        // console.log(cards)
+        
+        // $('.player-card').on('click', () => {
+        //     console.log($(event.target).attr('src'))
+        //     pChoice = cards.find(({imageUrl}) => imageUrl === $(event.target).attr('src')) //Inspiration for this method was found at https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find
+        //     console.log(pChoice)
+        // })
+        choosePoke()
+        
+    })
+ }
+}
+    // console.log('hello')
+    
+const choosePoke = () => {
+    $('.player-card').on('click', () => {
+        console.log($(event.target).attr('src'))
+        pChoice = cards.find(({imageUrl}) => imageUrl === $(event.target).attr('src')) //Inspiration for this method was found at https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find
+        console.log(pChoice)
     })
 }
+//// Testing variable idea
+$('button').on('click', () => {
+    startGame()
+    // choosePoke()
+})
 })
