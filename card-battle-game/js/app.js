4458f891096469b7bf506de8b8feadf71a3af6ed
Jeremy <jeremy.spewock@gmail.com>
2020-10-12 10:31:21 -0400

    generated 10 random cards

--- card-battle-game/js/app.js
+++ card-battle-game/js/app.js
@@ -1,13 +1,17 @@
 console.log('connected')
-let dexNum = Math.ceil(Math.random()*890)
 $(() => {
+    const cards = []
     console.log('j query connected')
+    for (let i = 0; i < 10; i++) {
+    let dexNum = Math.ceil(Math.random()*890)
     $.ajax({
         url: `https://api.pokemontcg.io/v1/cards?nationalPokedexNumber=${dexNum}`
     }).then((data) => {
         console.log(data)
-        $('body').html(
-            `<img src="${data.cards[0].imageUrl}">`
-        )
+        cards.push(data.cards[0])
+        let card = $('<img>').attr('src', data.cards[0].imageUrl)
+        $('body').append(card)
+        console.log(cards)
     })
+}
 })
